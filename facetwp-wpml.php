de80ff2b75d59f05b274d688635253af9e0db32f
mgibbs189 <mgibbs189@gmail.com>
2014-04-03 11:57:01 -0400

    Comments cleanup

--- facetwp-wpml.php
+++ facetwp-wpml.php
@@ -3,26 +3,12 @@
 Plugin Name: FacetWP - WPML
 Plugin URI: https://facetwp.com/
 Description: WPML support for FacetWP
-Version: 1.0.0
+Version: 1.0.1
 Author: Matt Gibbs
 Author URI: https://facetwp.com/
 GitHub Plugin URI: https://github.com/mgibbs189/facetwp-wpml
-GitHub Branch: 1.0.0
-
-Copyright 2014 Matt Gibbs
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+GitHub Branch: 1.0.1
+License: GPLv2
 */
 
 // exit if accessed directly
@@ -39,17 +25,13 @@ class FWP_WPML
     }
 
 
-    /**
-     * Intialize
-     */
+    // Intialize
     function init() {
         add_filter( 'facetwp_indexer_query_args', array( $this, 'facetwp_indexer_query_args' ) );
     }
 
 
-    /**
-     * Index all languages
-     */
+    // Index all languages
     function facetwp_indexer_query_args( $args ) {
         $args['suppress_filters'] = true; // query posts in all languages
         return $args;

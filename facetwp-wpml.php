8a4a32a62c5cd02583720afbcbb60e82aaafed55
mgibbs189 <mgibbs189@gmail.com>
2014-06-26 17:10:34 -0400

    Proper WPML language detection fix

--- facetwp-wpml.php
+++ facetwp-wpml.php
@@ -3,11 +3,11 @@
 Plugin Name: FacetWP - WPML
 Plugin URI: https://facetwp.com/
 Description: WPML support for FacetWP
-Version: 1.0.2
+Version: 1.0.3
 Author: Matt Gibbs
 Author URI: https://facetwp.com/
 GitHub Plugin URI: https://github.com/mgibbs189/facetwp-wpml
-GitHub Branch: 1.0.2
+GitHub Branch: 1.0.3
 License: GPLv2
 */
 
@@ -27,8 +27,33 @@ class FWP_WPML
 
     // Intialize
     function init() {
+        add_action( 'wp_footer', array( $this, 'wp_footer' ), 30 );
+        add_filter( 'facetwp_query_args', array( $this, 'facetwp_query_args' ), 10, 2 );
         add_filter( 'facetwp_indexer_query_args', array( $this, 'indexer_query_args' ) );
         add_action( 'facetwp_indexer_post', array( $this, 'set_post_langcode' ) );
+    }
+
+
+    /**
+     * Put the language into FWP_HTTP
+     */
+    function wp_footer() {
+        if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
+            $lang = ICL_LANGUAGE_CODE;
+            echo "<script>FWP_HTTP.lang = '$lang';</script>";
+        }
+    }
+
+
+    /**
+     * Query posts for the current language
+     */
+    function facetwp_query_args( $args, $class ) {
+        if ( isset( $class->http_params['lang'] ) ) {
+            global $sitepress;
+            $sitepress->switch_lang( $class->http_params['lang'] );
+        }
+        return $args;
     }
 
 
